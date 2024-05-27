791e86c9835e5c423bcafd4f6cc7749959060629
WangariNelly <nellywangari30@gmail.com>
2024-05-27 12:17:50 +0300

    Progress Bar

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ const menuToggle = document.getElementById("menuToggle");
 const menuHamburger = document.getElementById("menuHamburger");
 const radioButton = document.getElementsByName("inputName");
 const labelModal = document.querySelectorAll(".label--modal");
-const totalElement= document.getElementById('total');
-const totalBackers= document.getElementById('backers'); 
+const totalElement = document.getElementById("total");
+const totalBackers = document.getElementById("backers");
+const availableSlots = document.querySelectorAll(".slots");
+const progressBar = document.querySelector(".progressbar .file");
 
 //opens the modal
 openModal.onclick = function () {
@@ -37,24 +39,21 @@ gotIt.onclick = function () {
 let bookmarked = false;
 
 bookmark.addEventListener("click", () => {
-  console.log('bookmarked')
+  console.log("bookmarked");
   bookmarked = !bookmarked;
   bookmarked
     ? (bookmark_text.innerText = "Bookmarked")
     : (bookmark_text.innerText = "Bookmark");
 });
 
- 
-
 //radio buttons functionality
 
 for (let i = 0; i < radioButton.length; i++) {
   radioButton[i].addEventListener("change", () => {
-
     for (let j = 0; j < labelModal.length; j++) {
-     labelModal[j].style.display = "none";
+      labelModal[j].style.display = "none";
     }
-    
+
     if (radioButton[i].checked) {
       labelModal[i].style.display = "block";
     }
@@ -63,54 +62,94 @@ for (let i = 0; i < radioButton.length; i++) {
 
 //menu
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.style.display = 'none';
-  closeMenu.style.display = 'block';
-  menuHamburger.style.display = 'block';
+menuToggle.addEventListener("click", () => {
+  menuToggle.style.display = "none";
+  closeMenu.style.display = "block";
+  menuHamburger.style.display = "block";
 });
 
-closeMenu.addEventListener('click', () => {
-  closeMenu.style.display = 'none';
-  menuToggle.style.display = 'block';
-  menuHamburger.style.display = 'none';
+closeMenu.addEventListener("click", () => {
+  closeMenu.style.display = "none";
+  menuToggle.style.display = "block";
+  menuHamburger.style.display = "none";
 });
-
 
 const labelModals = document.querySelectorAll(".label--modal");
 const continueBtns = document.querySelectorAll(".continueBtn");
 
-let totalPledgeAmount = 89914;  
+let targetAmount = 100000;
+let totalPledgeAmount = 89914;
 let backers = 5007;
+let initialSlots = availableSlots.textContent;
+
+function updateProgressBar() {
+  const progressPercentage = Math.min((totalPledgeAmount / targetAmount) * 100, 100);
+  progressBar.textContent = `${progressPercentage.toFixed(1)}%`;
+}
+
+updateProgressBar();
+
 
 labelModals.forEach((labelModal, index) => {
   const continueBtn = continueBtns[index];
   continueBtn.addEventListener("click", enterPledge);
 
   function enterPledge(event) {
-    event.preventDefault(); 
+    event.preventDefault();
     const inputElement = labelModal.querySelector(".pledgeAmount");
-    const value = parseFloat(inputElement.value); 
-   
-    totalPledgeAmount += value;
-    backers++
-//local storage
+    const value = parseFloat(inputElement.value);
+    //local storage
     const storedPledges = JSON.parse(localStorage.getItem("pledges") || "[]");
     storedPledges.push(value);
     localStorage.setItem("pledges", JSON.stringify(storedPledges));
-    localStorage.setItem("totalPledgeAmount", totalPledgeAmount.toFixed(0));
-    localStorage.setItem("totalBackers", backers.toFixed(0));
-
-    totalBackers.textContent = `$${backers.toFixed(0)}`
-    totalElement.textContent = `$${totalPledgeAmount.toFixed(0)}`; 
     console.log(localStorage.getItem("pledges"));
+
+    //total pledge
+    totalPledgeAmount += value;
+
+    //stores Total pledged
+    const storedTotal = JSON.parse(
+      localStorage.getItem("totalPledgeAmount") || "[]"
+    );
+    storedPledges.push(storedTotal);
+    localStorage.setItem(
+      "totalPledgeAmount",
+      JSON.stringify(totalPledgeAmount.toFixed(0))
+    );
+    totalElement.textContent = `$${totalPledgeAmount.toFixed(0)}`;
+
+    //total backers
+    backers++;
+    //stores the total backers
+    localStorage.setItem("totalBackers", JSON.stringify(backers.toFixed(0)));
+    totalBackers.textContent = `$${backers.toFixed(0)}`;
+    // remaining slots
+    initialSlots--;
+    localStorage.setItem(
+      "slotsAvailable",
+      JSON.stringify(initialSlots.toFixed(0))
+    );
+    availableSlots.textContent = `$${initialSlots.toFixed(0)}`;
+    // if (initialSlots <= 0) {
+    //   labelModal.classList.add("inactive");
 
     modal.style.display = "none";
     thankyouModal.style.display = "block";
   }
 });
-
 // function setBackToDefault(){
 //   inputElement.value =""; // input empty
 //   continueBtn.textContent = "submit";
 //   labelModal.style.display = "none";
 // }
+
+//countdown for days
+
+function countDown() {
+  const today = new Date();
+  const futureDate = new Date(today.getFullYear(), 7, 30);
+  const daysDiffrence = futureDate - today;
+  const daysRemaining = Math.ceil(daysDiffrence / (1000 * 60 * 60 * 24));
+  document.getElementById("days").textContent = daysRemaining;
+}
+countDown();
